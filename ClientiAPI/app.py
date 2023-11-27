d2e649ca69a60abee176c8a7def775d0a15cf891
Federico <federico.neri@edu.itspiemonte.it>
2023-11-27 17:20:32 +0100

    Modificate le API

--- ClientiAPI/app.py
+++ ClientiAPI/app.py
@@ -1,25 +1,26 @@
 from flask import Flask, jsonify
-import psycopg2
+import psycopg2.pool
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-# Database connection parameters
-db_params = {
-    'dbname': 'UserDB',
-    'user': 'fede',
-    'password': 'mypassword',
-    'host': 'ContainerClienti',
-    'port': '5432',
-}
+# Create a connection pool
+connection_pool = psycopg2.pool.SimpleConnectionPool(
+    minconn=1,
+    maxconn=10,
+    dbname='UserDB',
+    user='fede',
+    password='mypassword',
+    host='0.0.0.0',
+    port='6000'
+)
 
 # API endpoint to retrieve data from the 'Clienti' table
 @app.route('/api/clients', methods=['GET'])
 def get_clients():
+    connection = connection_pool.getconn()
     try:
-        # Establish a connection to the PostgreSQL database
-        connection = psycopg2.connect(**db_params)
-
-        # Create a cursor object to interact with the database
         cursor = connection.cursor()
 
         # Select data from the 'Clienti' table
@@ -27,9 +28,9 @@ def get_clients():
         cursor.execute(select_query)
         data = cursor.fetchall()
 
-        # Close the cursor and connection
+        # Close the cursor (will return the connection to the pool) and commit changes
         cursor.close()
-        connection.close()
+        connection.commit()
 
         # Convert the data to JSON and return it
         return jsonify({'clients': data})
@@ -37,5 +38,9 @@ def get_clients():
     except Exception as e:
         return jsonify({'error': str(e)})
 
+    finally:
+        # Always return the connection to the pool
+        connection_pool.putconn(connection)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
