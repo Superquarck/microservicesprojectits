b7a92229f1e65e1392601be20db9a7232c36aca2
Federico <federico.neri@edu.itspiemonte.it>
2023-11-27 13:35:31 +0100

    Creato il DB e i dati all'interno. Creato l'immagine e il conseguente container.

--- UserDB/init.sql
+++ UserDB/init.sql
@@ -1,13 +1,17 @@
 -- Create a new database called 'UserDB'
-CREATE DATABASE UserDB;
-
-Create TABLE Clienti
+CREATE DATABASE "UserDB";
+\c "UserDB";
 
 CREATE TABLE Clienti (
 
-ID int,
+ID serial PRIMARY KEY,
 Nome varchar(255),
 Cognome varchar(255),
 Indirizzo varchar(255),
 Citta varchar(255)
 );
+
+INSERT INTO Clienti (Nome, Cognome, Indirizzo, Citta) VALUES
+    ('John', 'Doe', '123 Main St', 'City1'),
+    ('Jane', 'Smith', '456 Oak St', 'City2'),
+    ('Bob', 'Johnson', '789 Maple St', 'City3'); 
