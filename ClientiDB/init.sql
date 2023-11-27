0ebd91a75a976d1362b1c8a9b8b88ac9d033cde5
Federico <federico.neri@edu.itspiemonte.it>
2023-11-27 17:00:28 +0100

    modificato l'init.sql

--- ClientiDB/init.sql
+++ ClientiDB/init.sql
@@ -1,17 +1,17 @@
--- Create a new database called 'UserDB'
-CREATE DATABASE "UserDB";
+-- Connect to the existing database (replace "UserDB" with your actual database name)
 \c "UserDB";
 
-CREATE TABLE Clienti (
-
-ID serial PRIMARY KEY,
-Nome varchar(255),
-Cognome varchar(255),
-Indirizzo varchar(255),
-Citta varchar(255)
+-- Create the "Clienti" table
+CREATE TABLE IF NOT EXISTS Clienti (
+    ID serial PRIMARY KEY,
+    Nome varchar(255),
+    Cognome varchar(255),
+    Indirizzo varchar(255),
+    Citta varchar(255)
 );
 
+-- Insert data into the "Clienti" table
 INSERT INTO Clienti (Nome, Cognome, Indirizzo, Citta) VALUES
     ('John', 'Doe', '123 Main St', 'City1'),
     ('Jane', 'Smith', '456 Oak St', 'City2'),
-    ('Bob', 'Johnson', '789 Maple St', 'City3'); 
+    ('Bob', 'Johnson', '789 Maple St', 'City3');
