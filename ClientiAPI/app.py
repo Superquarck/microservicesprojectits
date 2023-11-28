86dd210c1484663e9345f9325c69793ea08faffc
Federico <federico.neri@edu.itspiemonte.it>
2023-11-28 12:35:09 +0100

    sclero

--- ClientiAPI/app.py
+++ ClientiAPI/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.urls import quote
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://myuser:mypassword@postgres:5432/mydatabase'
