8f5edad48f6c2c0eef60bd1c9a1b41f6402a3a38
Federico <federico.neri@edu.itspiemonte.it>
2023-11-28 11:06:24 +0100

    definiti dei nomi

--- ClientiAPI/app.py
+++ ClientiAPI/app.py
@@ -12,7 +12,7 @@ connection_pool = psycopg2.pool.SimpleConnectionPool(
     dbname='UserDB',
     user='fede',
     password='mypassword',
-    host='0.0.0.0',
+    host='clientidata',
     port='5432'
 )
 
@@ -43,4 +43,4 @@ def get_clients():
         connection_pool.putconn(connection)
 
 if __name__ == '__main__':
-    app.run(debug=True, host=("0.0.0.0"), port=5432)
+    app.run(debug=True, host=("clientidata"), port=5432)
