8a1053c2a3ed6f83a49612ce99ce1810b64e95de
Federico <federico.neri@edu.itspiemonte.it>
2023-11-29 19:17:17 +0100

    Modificato il docker compose e quasi finito

---
+++ LibriAPI/app.py
@@ -0,0 +1,141 @@
+from flask import Flask, jsonify, request
+import psycopg2.pool
+from flask_cors import CORS
+import time 
+
+time.sleep(30)
+
+app = Flask(__name__)
+CORS(app)
+
+# Create a connection pool
+connection_pool = psycopg2.pool.SimpleConnectionPool(
+    minconn=1,
+    maxconn=10,
+    dbname='LibriDB',
+    user='fede',
+    password='mypassword',
+    host='clientidata',
+    port='5432'
+)
+
+# API endpoint to retrieve data from the 'Libri' table
+@app.route('/libri', methods=['GET'])
+def get_clients():
+    connection = connection_pool.getconn()
+    try:
+        cursor = connection.cursor()
+
+        # Select data from the 'Libri' table
+        select_query = "SELECT * FROM Libri;"
+        cursor.execute(select_query)
+        data = cursor.fetchall()
+
+        # Close the cursor (will return the connection to the pool) and commit changes
+        cursor.close()
+        connection.commit()
+
+        # Convert the data to JSON and return it
+        return jsonify({'clienti': data})
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+    finally:
+        # Always return the connection to the pool
+        connection_pool.putconn(connection)
+
+@app.route('/libri', methods=['POST'])
+def create_client():
+    connection = connection_pool.getconn()
+    try:
+        cursor = connection.cursor()
+
+        # Extract data from the request
+        id = request.json.get('ID')
+        titolo = request.json.get('Titolo')
+        editore = request.json.get('Editore')
+        genere = request.json.get('Genere')
+        autore = request.json.get('Autore')
+
+        # Insert data into the 'Libri' table
+        insert_query = "INSERT INTO Libri (ID, Titolo, Editore, Genere, Autore) VALUES (%s, %s, %s, %s, %s) RETURNING *;"
+        cursor.execute(insert_query, (id, titolo, editore, genere, autore))
+        new_client = cursor.fetchone()
+
+        # Commit changes
+        connection.commit()
+
+        # Close the cursor (will return the connection to the pool) and return the new client data
+        cursor.close()
+        return jsonify({'clienti': new_client}), 201
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+    finally:
+        # Always return the connection to the pool
+        connection_pool.putconn(connection)
+
+# Update operation
+@app.route('/libri/<int:libri_id>', methods=['PUT'])
+def update_client(libri_id):
+    connection = connection_pool.getconn()
+    try:
+        cursor = connection.cursor()
+
+        # Extract data from the request
+        titolo = request.json.get('Titolo')
+        editore = request.json.get('Editore')
+        genere = request.json.get('Genere')
+        autore = request.json.get('Autore')
+
+        # Update data in the 'Libri' table
+        update_query = "UPDATE Libri SET Titolo = %s, Editore = %s, Genere = %s, Autore = %s WHERE id = %s RETURNING *;"
+        cursor.execute(update_query, (titolo, editore, genere, autore, libri_id_id))
+        updated_client = cursor.fetchone()
+
+        # Commit changes
+        connection.commit()
+
+        # Close the cursor (will return the connection to the pool) and return the updated client data
+        cursor.close()
+        return jsonify({'clienti': updated_client})
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+    finally:
+        # Always return the connection to the pool
+        connection_pool.putconn(connection)
+
+# Delete operation
+@app.route('/clienti/<int:libri_id>', methods=['DELETE'])
+def delete_client(libri_id):
+    connection = connection_pool.getconn()
+    try:
+        cursor = connection.cursor()
+
+        # Delete data from the 'Clienti' table
+        delete_query = "DELETE FROM Clienti WHERE id = %s RETURNING *;"
+        cursor.execute(delete_query, (libri_id,))
+        deleted_client = cursor.fetchone()
+
+        # Commit changes
+        connection.commit()
+
+        # Close the cursor (will return the connection to the pool) and return the deleted client data
+        cursor.close()
+        return jsonify({'clienti': deleted_client})
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+    finally:
+        # Always return the connection to the pool
+        connection_pool.putconn(connection)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=4999)
--- LibriDB/nit.sql
+++ LibriDB/nit.sql
@@ -1,8 +1,8 @@
 -- Connect to the existing database (replace "UserDB" with your actual database name)
 \c "LibriDB";
 
--- Create the "Clienti" table
-CREATE TABLE IF NOT EXISTS Clienti (
+-- Create the "Libri" table
+CREATE TABLE IF NOT EXISTS Libri (
     ID serial PRIMARY KEY,
     Titolo varchar(255),
     Editore varchar(255),
@@ -10,8 +10,8 @@ CREATE TABLE IF NOT EXISTS Clienti (
     Autore VARCHAR(255)
 );
 
--- Insert data into the "Clienti" table
-INSERT INTO Clienti (ID, Titolo, Editore, Genere, Autore) VALUES
+-- Insert data into the "Libri" table
+INSERT INTO Libri (ID, Titolo, Editore, Genere, Autore) VALUES
     ('1', 'La via del massacro', 'Mondadori', 'Thriller', 'Io'),
     ('2', 'Tre uomini e una gamba', 'Feltrinelli', 'Horror', 'Tu'),
     ('3', 'Dio', 'Mondadori', 'Horror', 'Loro');
